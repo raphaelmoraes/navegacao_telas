7ad2456675e53cfaab094d77e9e20b18e432dfd3
Raphael <viladapenha@gmail.com>
2021-06-21 18:12:48 -0300

    Navegação entre Telas

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:navegacao/TelaSecundaria.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: TelaPrincipal(),
+  ));
+}
+
+class TelaPrincipal extends StatefulWidget {
+  const TelaPrincipal({Key? key}) : super(key: key);
+
+  @override
+  _TelaPrincipalState createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tela Principal"),
+        backgroundColor: Colors.orange,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (cotext) => TelaSecundaria(),
+                  ),
+                );
+              },
+              child: Text("Ir para segunda tela"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
